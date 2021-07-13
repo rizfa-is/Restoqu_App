058638b439e34252fe6f38ecf2e8152d1f9db2e4
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2021-07-13 15:25:24 +0700

    Replace App.js into src folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Button from "./src/components/atoms/Button";
+import React from 'react';
+import Button from './components/atoms/Button';
 
 const App = () => {
   return <Button text="Ini Tombol" />;
