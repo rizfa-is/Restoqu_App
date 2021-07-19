012786f5f22aa256d472e3249559106d8f3a7644
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2021-07-19 20:11:56 +0700

    Customize Sign In page Add TextInput component Add router for navigation

--- src/components/atoms/TextInput/index.js
+++ src/components/atoms/TextInput/index.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput as TextInputRN } from 'react-native'
+import Gap from '../Gap'
 
-const TextInput = () => {
+const TextInput = ({title, placeholder}) => {
     return (
         <View>
-            <Text>Text Input Component</Text>
+            <Text style={styles.text}>{title}</Text>
+            <Gap height={6}/>
+            <TextInputRN placeholder={placeholder} style={styles.box}/>
         </View>
     )
 }
 
 export default TextInput
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    text: {
+        fontFamily: 'Poppins-Regular',
+        fontSize: 16,
+        color: '#020202'
+    },
+    box: {
+        borderColor: '#020202',
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 10
+    }
+})
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Button, Gap, Header, TextInput } from '../../components'
 
 const SignIn = () => {
@@ -8,10 +8,13 @@ const SignIn = () => {
             <Header title='Sign In' subtitle='Find your best ever meal'/>
             <Gap height={24} backgroundColor={'#e2e2e2'}/>
             <View style={styles.container}>
-                <TextInput/>
-                <TextInput/>
-                <Button/>
-                <Button/>
+                <TextInput title='Email' placeholder='Type your email address'/>
+                <Gap height={16}/>
+                <TextInput title='Password' placeholder='Type your password'/>
+                <Gap height={24}/>
+                <Button text='Sign In'/>
+                <Gap height={12}/>
+                <Button text='Create New Account' textColor='white' color='#8D92A3'/>
             </View>
         </View>
     )
